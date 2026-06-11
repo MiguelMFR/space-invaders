d119c8f2b687aa2f29a921306f6f779fd41dff47
MiguelMFR <guelferreirar@gmail.com>
2026-06-11 18:33:08 -0300

    feat: Add game assets including images and sound effects

---
+++ sound.py
@@ -0,0 +1,15 @@
+from PPlay.sound import Sound
+
+# SONS
+som_tiro = Sound("sounds/shoot-sound.ogg")
+som_tiro.set_volume(80)
+som_game_over = Sound("sounds/game-over-sound.ogg")
+som_game_over.set_volume(80)
+som_click = Sound("sounds/click-sound.ogg")
+som_click.set_volume(80)
+som_menu = Sound("sounds/menu-sound.ogg")
+som_menu.set_volume(80)
+som_menu.loop = True
+som_jogo = Sound("sounds/jogo-sound.ogg")
+som_jogo.set_volume(80)
+som_jogo.loop = True
--- sprites.py
+++ sprites.py
@@ -1,4 +1,5 @@
-from PPlay.sprite import *
+import random
+from PPlay.sprite import Sprite
 import config
 
 # MENU
@@ -57,3 +58,30 @@ background_menu = Sprite("images/background-menu.png", 1)
 background_menu.set_position(0, 0)
 background_dif = Sprite("images/background-dif.png", 1)
 background_dif.set_position(0, 0)
+background_jogo = Sprite("images/background-jogo.png", 1)
+background_jogo.set_position(0, 0)
+background_rank = Sprite("images/background-dif.png", 1)
+background_rank.set_position(0, 0)
+# NAVE
+
+nave = Sprite("images/nave.png", 1)
+tiro_nave = Sprite("images/nave_tiro.png", 1)
+nave.set_position(config.window.width/2 - nave.width/2, config.window.height - nave.height)
+vida = Sprite("images/vida.png", 1)
+
+# MONSTROS
+
+polvo = Sprite("images/polvo.png", 1)
+caranguejo = Sprite("images/caranguejo.png", 1)
+
+# GAME OVER
+game_over = Sprite("images/game-over.png", 1)
+game_over.set_position(config.window.width/2 - game_over.width/2, config.window.height/2 - game_over.height/2)
+press_enter_or_esc = Sprite("images/game-over-comp.png", 1)
+press_enter_or_esc.set_position(config.window.width/2 - press_enter_or_esc.width/2, config.window.height/2 + game_over.height/2)
+
+# VOLUME 
+volume_on = Sprite("images/som-ligado.png", 1)
+volume_on.set_position(config.window.width - volume_on.width - 10, 10)
+volume_off = Sprite("images/som-desligado.png", 1)
+volume_off.set_position(config.window.width - volume_off.width - 10, 10)
